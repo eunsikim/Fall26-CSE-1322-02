c4eaca8af4210acbd395301b76a3dfd964dbc238
eunsikim <kimeunsik229@gmail.com>
2026-08-26 13:36:18 -0400

    in class 08/26

---
+++ Week 1/java_crash.java
@@ -0,0 +1,11 @@
+public class java_crash {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        int[] my_array = new int[1];
+
+        System.out.println(my_array[10]);
+
+        System.out.println("World");
+    }
+}
---
+++ Week 1/python_crash.py
@@ -0,0 +1,8 @@
+print("Hello")
+print("CSE 1322")
+
+my_list = []
+
+print(my_list[0])
+
+print("World")
